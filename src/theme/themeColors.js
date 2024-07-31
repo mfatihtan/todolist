dfc139d648d2386545a84a99e510074dce43dcf7
mfatihtan <mfatihtan23@gmail.com>
2024-07-31 13:00:34 +0300

    First commit

---
+++ src/theme/themeColors.js
@@ -0,0 +1,8 @@
+const themeColors = {
+    blue: "#7CB9E8",
+    lightBlue: "#F0F8FF",
+    darkBlue: "#4A6F8B",
+    black: "#000000",
+    white: "#ffffff",
+    blueRGBA: "rgba(124, 185, 232,0.3)",
+}
